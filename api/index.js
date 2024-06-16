29ae50de67dc902c3ae7267024c94b1a7aba9227
Frnn4268 <appstutorialesy@gmail.com>
2024-06-15 20:20:51 -0600

    Adding new changes in backend

--- api/index.js
+++ api/index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const Transaction = require('./models/Transaction.js');
 const mongoose = require("mongoose")
+
+require('dotenv').config();
+
+const Transaction = require('./models/Transaction.js');
+
 const app = express();
-const port = 4000
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World F1.F2!');
-})
 
 app.post('/api/transaction', async (req, res) => {
     await mongoose.connect(process.env.MONGO_URL);
@@ -26,6 +24,8 @@ app.get('/api/transaction', async (req, res) => {
     res.json(transactions);
   })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const PORT = process.env.BACKEND_PORT || 4000
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 })
